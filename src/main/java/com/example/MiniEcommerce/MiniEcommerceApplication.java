02a0a75235b735501714900fc9aca4cd6c50120e
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-01-09 19:28:20 +0530

    first commit

---
+++ src/main/java/com/example/MiniEcommerce/MiniEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.MiniEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MiniEcommerce/MiniEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MiniEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
