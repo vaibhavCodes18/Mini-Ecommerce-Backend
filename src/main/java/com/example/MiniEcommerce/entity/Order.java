397db19274dcbce03d16a68e8080910cb24a94c2
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-01-09 22:24:01 +0530

    User entity done

---
+++ src/main/java/com/example/MiniEcommerce/entity/Order.java
@@ -0,0 +1,4 @@
+package com.example.MiniEcommerce.entity;
+
+public class Order {
+}
---
+++ src/main/java/com/example/MiniEcommerce/entity/User.java
@@ -0,0 +1,55 @@
+package com.example.MiniEcommerce.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Order> orders;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+}
