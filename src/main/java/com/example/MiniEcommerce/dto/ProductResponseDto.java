4d05d756ed68edabf20591369075af46f81e78fd
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-01-10 13:29:56 +0530

    product request & response dto added

--- src/main/java/com/example/MiniEcommerce/dto/ProductResponseDto.java
+++ src/main/java/com/example/MiniEcommerce/dto/ProductResponseDto.java
@@ -4,4 +4,31 @@ public class ProductResponseDto {
     private Long productId;
     private String productName;
     private Integer price;
+
+    public ProductResponseDto() {
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
 }
